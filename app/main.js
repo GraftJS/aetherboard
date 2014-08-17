97c5dba5a1e4b5e552b11e20ef58e3d3af983d2d
Adrian Rossouw <adrian@daemon.co.za>
2014-08-17 12:31:18 +0200

    The start of some canvas based interface.
    nothing spectacular yet.

--- app/main.js
+++ app/main.js
@@ -11,24 +11,16 @@ require('./index.html');
 var Engine = require('famous/core/Engine');
 var Modifier = require('famous/core/Modifier');
 var Transform = require('famous/core/Transform');
-var ImageSurface = require('famous/surfaces/ImageSurface');
+var Whiteboard = require('./whiteboard');
 
 // create the main context
 var mainContext = Engine.createContext();
 
-// your app here
-var logo = new ImageSurface({
-  size: [200, 200],
-  content: 'https://rawgit.com/GraftJS/graft.io/master/static/images/graft-icon.png',
-  classes: ['double-sided']
-});
-
-var initialTime = Date.now();
-var centerSpinModifier = new Modifier({
+var rootModifier = new Modifier({
   origin: [0.5, 0.5],
-  transform : function(){
-    return Transform.rotateY(0.002 * (Date.now() - initialTime));
-  }
+  size: function() { return mainContext.getSize(); }
 });
 
-mainContext.add(centerSpinModifier).add(logo);
+var whiteboard = new Whiteboard();
+
+mainContext.add(rootModifier).add(whiteboard);
---
+++ app/whiteboard.js
@@ -0,0 +1,42 @@
+var CanvasSurface = require('famous/surfaces/CanvasSurface');
+
+function Whiteboard(options) {
+    CanvasSurface.apply(this, arguments);
+
+    var ctx = this.getContext('2d');
+    ctx.fillStyle = "solid";
+    ctx.strokeStyle = "#bada55";
+    ctx.lineWidth = 5;
+    ctx.lineCap = "round";
+
+    // simplest way i could think of.
+    // go look here first :
+    // http://famo.us/docs/0.2.0/inputs/GenericSync/
+    // http://famo.us/docs/0.2.0/inputs/MouseSync
+    this.on('mousedown', drawEvent.bind(this, 'start'));
+    this.on('mousemove', drawEvent.bind(this, 'move'));
+    this.on('mouseup', drawEvent.bind(this, 'end'));
+
+    function drawEvent(type, evt) {
+        this.draw(evt.offsetX, evt.offsetY, type);
+    }
+}
+
+Whiteboard.prototype = Object.create(CanvasSurface.prototype);
+Whiteboard.prototype.constructor = Whiteboard;
+
+Whiteboard.prototype.draw = function(x, y, type) {
+    var ctx = this.getContext('2d');
+    if (type === "start") {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else if (type === "move") {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    } else {
+        ctx.closePath();
+    }
+};
+
+
+module.exports = Whiteboard;
